88a113753b6d9320a50ad307c62d9abd826d3a42
Johnathon Selstad <makeshifted@gmail.com>
2026-04-11 17:15:35 -0700

    Updates

---
+++ assets/PhysicalSpotLight-8mrCxqB-.js
@@ -0,0 +1,2 @@
+import{S as t}from"./three.module-Blrp6k_f.js";class r extends t{constructor(...s){super(...s),this.iesMap=null,this.radius=0}copy(s,i){return super.copy(s,i),this.iesMap=s.iesMap,this.radius=s.radius,this}}export{r as P};
+//# sourceMappingURL=PhysicalSpotLight-8mrCxqB-.js.map
---
+++ assets/tinybvh-DYMH03hg.js
@@ -0,0 +1,2 @@
+const t=""+new URL("libultrahdr-CQI41pni.js",import.meta.url).href,_=""+new URL("tinybvh-C1DjSZqX.js",import.meta.url).href,e=""+new URL("tinybvh-Bcb5Wmcw.wasm",import.meta.url).href;export{e as _,_ as a,t as b};
+//# sourceMappingURL=tinybvh-DYMH03hg.js.map
